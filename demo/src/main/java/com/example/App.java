fab6bfc27461a0e9bb7289e304a8fd404e8c2d1d
SLealOmana <leal.sa982@gmail.com>
2022-11-27 16:43:09 -0500

    estres

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import javafx.stage.StageStyle;
 
 import javafx.scene.image.Image;
 
