62f7e93ec0fb009a9f66a1ca8dd4f2d5005fa6ee
SLealOmana <leal.sa982@gmail.com>
2022-11-27 16:42:53 -0500

    estres

--- demo/src/main/java/com/example/ArmarioController.java
+++ demo/src/main/java/com/example/ArmarioController.java
@@ -1,21 +1,37 @@
 package com.example;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import javax.imageio.ImageIO;
+
+import com.google.gson.Gson;
+
+import java.awt.image.BufferedImage;
+import javafx.scene.image.Image;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
-
 
 public class ArmarioController
 {
+    @FXML
+    void  boton_anterior(ActionEvent event) throws IOException{
+        actualizarPantalla(1);
+    }
+
+    @FXML
+    void  boton_siguiente (ActionEvent event) throws  IOException{
+        actualizarPantalla(-1);
+    }
+    @FXML
+    private ImageView display;
     
     @FXML
     private Button button_Armario;
-
-    @FXML
-    private Button button_Camara;
 
     @FXML
     private Button button_Inicio;
@@ -26,7 +42,19 @@ public class ArmarioController
     private Button button_add;
     @FXML
     private Button button_search;
-    @FXML
+    void actualizarPantalla (int cont){
+        Gson gson = new Gson();
+/* 
+            if (cont+1<productos.size()){
+                cont++;
+                p1_imagen.setImage(productos.get(cont).getImagenProducto());
+                p1_name.setText(productos.get(cont).getNombreProducto());
+                p1_descripcion.setText(productos.get(cont).getDescripcionProducto());
+                p1_precio.setText((String.valueOf(productos.get(cont).getPrecioProducto())));
+            }*/
+
+    }
+@FXML
     void add_prenda(ActionEvent event) throws IOException{
         FileChooser fil_chooser = new FileChooser();
         FileChooser.ExtensionFilter extFilter = 
@@ -35,7 +63,17 @@ public class ArmarioController
         File file = fil_chooser.showOpenDialog(null);
                 if (file != null) {
                     System.out.println(file.getAbsolutePath()+ "  selected");
+                    byte[] fileContent = Files.readAllBytes(file.toPath());
+                    ByteArrayInputStream inStreambj = new ByteArrayInputStream(fileContent);
+                    BufferedImage newImage = ImageIO.read(inStreambj);
+                    ImageIO.write(newImage, "jpg", new File("demo\\src\\main\\resources\\com\\example\\tmp\\outputImage.jpg"));
+                    File fl = new File("demo\\src\\main\\resources\\com\\example\\tmp\\outputImage.jpg");
+                    Image im = new Image(fl.toURI().toString());
+                    display.setImage(im);;
                 }
+        
+
+
     }
     @FXML
     void ir_Armario(ActionEvent event) throws IOException{
