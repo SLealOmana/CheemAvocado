f60d5d86144ae52710510a4375479ea4a34c6797
SLealOmana <leal.sa982@gmail.com>
2022-11-27 11:53:06 -0500

    FileFIlter

--- demo/src/main/java/com/example/ArmarioController.java
+++ demo/src/main/java/com/example/ArmarioController.java
@@ -4,17 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
 
 public class ArmarioController
 {
-    FileChooser fil_chooser = new FileChooser();
+    
     @FXML
     private Button button_Armario;
 
@@ -32,6 +28,10 @@ public class ArmarioController
     private Button button_search;
     @FXML
     void add_prenda(ActionEvent event) throws IOException{
+        FileChooser fil_chooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = 
+                        new FileChooser.ExtensionFilter("Img files (*.png,, *.jpg...)", "*.png", "*.jpg", "*.jpeg");
+                        fil_chooser.getExtensionFilters().add(extFilter);
         File file = fil_chooser.showOpenDialog(null);
                 if (file != null) {
                     System.out.println(file.getAbsolutePath()+ "  selected");
